2b9595c3e958cbd88dcfb6587a3df2db7929d4a1
FDULeolu <fdulyz@sina.com>
2024-02-27 19:44:54 +0800

    Lab11: Finish Astar

--- lab11/lab11/graphs/MazeAStarPath.java
+++ lab11/lab11/graphs/MazeAStarPath.java
@@ -1,5 +1,8 @@
 package lab11.graphs;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  *  @author Josh Hug
  */
@@ -8,6 +11,24 @@ public class MazeAStarPath extends MazeExplorer {
     private int t;
     private boolean targetFound = false;
     private Maze maze;
+
+    private class Node {
+        private int v;
+        private int priority;
+
+        public Node(int v) {
+            this.v = v;
+            this.priority = distTo[v] + h(v);
+        }
+    }
+
+    private class NodeComparator implements Comparator<MazeAStarPath.Node> {
+
+        @Override
+        public int compare(MazeAStarPath.Node o1, MazeAStarPath.Node o2) {
+            return o1.priority - o2.priority;
+        }
+    }
 
     public MazeAStarPath(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
@@ -20,7 +41,11 @@ public class MazeAStarPath extends MazeExplorer {
 
     /** Estimate of the distance from v to the target. */
     private int h(int v) {
-        return -1;
+        int sourceX = maze.toX(v);
+        int sourceY = maze.toY(v);
+        int targetX = maze.toX(t);
+        int targetY = maze.toY(t);
+        return Math.abs(sourceX - targetX) + Math.abs(sourceY - targetY);
     }
 
     /** Finds vertex estimated to be closest to target. */
@@ -31,7 +56,26 @@ public class MazeAStarPath extends MazeExplorer {
 
     /** Performs an A star search from vertex s. */
     private void astar(int s) {
-        // TODO
+        PriorityQueue<Node> pq = new PriorityQueue<>(new NodeComparator());
+        Node curNode = new Node(s);
+        pq.add(curNode);
+        marked[s] = true;
+        while (!pq.isEmpty()) {
+            curNode = pq.poll();
+            for (int w : maze.adj(curNode.v)) {
+                if (!marked[w]) {
+                    marked[w] = true;
+                    edgeTo[w] = curNode.v;
+                    distTo[w] = distTo[curNode.v] + 1;
+                    announce();
+                    if (w == t) {
+                        return;
+                    } else {
+                        pq.add(new Node(w));
+                    }
+                }
+            }
+        }
     }
 
     @Override
