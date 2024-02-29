7dfded184bdffa1fc87949873ba444aa300d0690
FDULeolu <fdulyz@sina.com>
2024-02-29 21:28:09 +0800

    Lab13: First submit

--- lab13/RadixSort.java
+++ lab13/RadixSort.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 /**
  * Class for doing Radix sort
  *
@@ -16,8 +18,32 @@ public class RadixSort {
      * @return String[] the sorted array
      */
     public static String[] sort(String[] asciis) {
-        // TODO: Implement LSD Sort
-        return null;
+        if (asciis.length <= 1) {
+            return asciis;
+        }
+        String max = asciis[0];
+        String[] sorted = new String[asciis.length];
+        for (int i = 0; i < asciis.length; i++) {
+            if (stringToAscii(max) < stringToAscii(asciis[i])) {
+                max = asciis[i];
+            }
+            sorted[i] = asciis[i];
+        }
+
+        for (int i = 1; stringToAscii(max) / i > 0; i *= 256) {
+            sortHelperLSD(sorted, i);
+        }
+
+        return sorted;
+    }
+
+    private static int stringToAscii(String s) {
+        int ascii = 0;
+        for (int i = 0; i < s.length(); i++) {
+            ascii *= 256;
+            ascii += s.charAt(i);
+        }
+        return ascii;
     }
 
     /**
@@ -28,7 +54,29 @@ public class RadixSort {
      */
     private static void sortHelperLSD(String[] asciis, int index) {
         // Optional LSD helper method for required LSD radix sort
-        return;
+        int max = Integer.MIN_VALUE;
+        for (String s : asciis) {
+            max = Math.max(max, (stringToAscii(s) / index) % 256);
+        }
+
+        int[] counts = new int[max + 1];
+        for (String s : asciis) {
+            counts[(stringToAscii(s) / index) % 256]++;
+        }
+
+        for (int i = 1; i < counts.length; i++) {
+            counts[i] += counts[i - 1];
+        }
+
+        String[] output = new String[asciis.length];
+        for (int i = 0; i < asciis.length; i++) {
+            output[counts[((stringToAscii(asciis[i])) / index ) % 256] - 1] = asciis[i];
+            counts[((stringToAscii(asciis[i])) / index ) % 256]--;
+        }
+
+        for (int i = 0; i < output.length; i++) {
+            asciis[i] = output[i];
+        }
     }
 
     /**
@@ -45,4 +93,14 @@ public class RadixSort {
         // Optional MSD helper method for optional MSD radix sort
         return;
     }
+
+    public static void main(String[] args) {
+        String[] sorted;
+        String[] strings = {"bdb", "ca", "aba"};
+        sorted = sort(strings);
+        for (String s : sorted) {
+            System.out.println(s);
+        }
+    }
+
 }
